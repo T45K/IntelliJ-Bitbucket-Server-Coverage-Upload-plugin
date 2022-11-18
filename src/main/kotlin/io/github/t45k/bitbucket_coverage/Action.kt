5ee5375afdbeca7dc83f5e725c1812c1a9dd2443
T45K <tasktas9@gmail.com>
2022-11-18 23:40:55 +0900

    init

---
+++ src/main/kotlin/io/github/t45k/bitbucket_coverage/Action.kt
@@ -0,0 +1,11 @@
+package io.github.t45k.bitbucket_coverage
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class Action : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
